aeb258ef91df2bc990fe726a47e8ae65b4168656
Zaqueu <zaqueu.tecnologia@gmail.com>
2025-09-15 17:41:42 -0300

    Add Models e troca da tela inicial

--- MeuAppMinhasCompras/Models/Produto.cs
+++ MeuAppMinhasCompras/Models/Produto.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
+
 
 namespace MeuAppMinhasCompras.Models
 {
-    internal class Produto
+    public class Produto
     {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
     }
 }
