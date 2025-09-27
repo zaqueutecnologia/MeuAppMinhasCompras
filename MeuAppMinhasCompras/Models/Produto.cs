b80e1aa47d1c4ee42f443415761dee13899a7d25
Zaqueu <zaqueu.tecnologia@gmail.com>
2025-09-26 23:17:55 -0300

    Validação da inserção de produto e correção dos produtos duplicados na tela inicial.

--- MeuAppMinhasCompras/Models/Produto.cs
+++ MeuAppMinhasCompras/Models/Produto.cs
@@ -4,9 +4,22 @@ namespace MeuAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey,AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if(value == null) 
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
